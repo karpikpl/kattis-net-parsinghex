6d701ae805f091cc5f57316ce6ecc1ddc37c0676
Piotr Karpala <pkarpala@infusion.com>
2017-10-19 00:29:54 -0400

    case 4 failing

--- KattisSolution/Program.cs
+++ KattisSolution/Program.cs
@@ -1,29 +1,54 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using System.Text.RegularExpressions;
 using KattisSolution.IO;
 
 namespace KattisSolution
 {
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            Solve(Console.OpenStandardInput(), Console.OpenStandardOutput());
-        }
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Solve(Console.OpenStandardInput(), Console.OpenStandardOutput());
+		}
 
-        public static void Solve(Stream stdin, Stream stdout)
-        {
-            IScanner scanner = new OptimizedPositiveIntReader(stdin);
-            // uncomment when you need more advanced reader
-            // IScanner scanner = new Scanner(stdin);
-            // IScanner scanner = new LineReader(stdin);
-            var writer = new BufferedStdoutWriter(stdout);
+		public static void Solve(Stream stdin, Stream stdout)
+		{
+			// IScanner scanner = new OptimizedPositiveIntReader(stdin);
+			// uncomment when you need more advanced reader
+			// IScanner scanner = new Scanner(stdin);
+			IScanner scanner = new LineReader(stdin);
+			var writer = new BufferedStdoutWriter(stdout);
+			var regex = new Regex("0[xX][\\da-fA-F]+");
 
-            var input = scanner.NextInt();
+			string input = scanner.Next();
 
-            writer.Write(input*5);
-            writer.Write("\n");
-            writer.Flush();
-        }
-    }
+			while (input != null)
+			{
+
+				var match = regex.Match(input);
+
+				while (match.Success)
+				{
+					int hex;
+					bool parsed = int.TryParse(match.Value.Substring(2), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out hex);
+
+					if (parsed)
+					{
+						writer.Write(match.Value);
+						writer.Write(" ");
+
+						writer.Write(hex);
+						writer.Write("\n");
+					}
+					match = match.NextMatch();
+				}
+
+				input = scanner.Next();
+			}
+
+			writer.Flush();
+		}
+	}
 }
